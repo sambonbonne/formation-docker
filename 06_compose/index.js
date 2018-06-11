16eddcc05f270e87dffb1b234c9dbe10f700bacc
Samuel DENIS <samuel@denis.tech>
2018-06-11 15:11:15 +0200

    Fix docker compose

--- 06_compose/index.js
+++ 06_compose/index.js
@@ -2,11 +2,11 @@
 
 const app = require('express')();
 const redis = require('redis');
-const client = redis.createClient();
+const client = redis.createClient('redis://db:6379');
 
 app
   .get('/get/:key', (req, res) => {
-    const { key } = req.path;
+    const { key } = req.params;
 
     client.get(key, (err, value) => {
       if (err)
@@ -14,11 +14,11 @@ app
       else if (!value)
         res.status(404).send();
       else
-        res.status(200).send(reply);
+        res.status(200).send(value);
     });
   })
   .get('/set/:key/:value', (req, res) => {
-    const { key, value } = req.path;
+    const { key, value } = req.params;
 
     client.set(key, value, redis.print)
     res.status(202).send();
@@ -32,5 +32,5 @@ client.on('error', err => {
   process.exit(1);
 })
 
-process.on('exit', () => app.close());
+// process.on('exit', () => app.close());
 app.listen(80, () => process.stdout.write('Server started\n'));
